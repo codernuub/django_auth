bd0712a3f0d2e9bbdb03a4d2605742112c983de3
codernuub <sardarbhai6@gmail.com>
2021-01-30 00:31:42 +0530

    done with login and registter

--- account/models.py
+++ account/models.py
@@ -1,8 +1,8 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.IntegerField(blank=True, primary_key=True)
+    _id = models.ObjectIdField()
     name = models.CharField(max_length=20,blank=False)
     email = models.EmailField(max_length=256, blank=False, unique=True)
     contact = models.BigIntegerField(blank=False,unique=True)
--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 from .utils import set_cookie, verify_token, generate_token, redirect_to_main
 
-main_site = 'http://127.0.0.1:3000'
+
 
 # register page
 def register(request):
@@ -15,7 +15,7 @@ def register(request):
 def login(request):
     # check token is valid redirect user to main application
     if verify_token(request.COOKIES.get('token')):
-        return redirect(main_site)
+        return redirect("127.0.0.1:3000")
 
     return render(request, 'account/login.html')
 
@@ -26,31 +26,37 @@ def registerUser(request):
     email = request.POST['email']
     contact = request.POST['contact']
     password = request.POST['password']
-    
+
     try:
-      user = User(name=name,email=email,contact=contact)
-      if password:
-         user.password = make_password(password)
-      user.full_clean()
-      id = user.save()._id
-      redirect_to_main(id, redirect)
+        user = User(name=name, email=email, contact=contact)
+        if password:
+            user.password = make_password(password)
+        user.full_clean()
+        user.save()
+        return redirect_to_main(user._id, redirect)
     except Exception as e:
-      print(e.args)
-      return render(request, 'account/register.html', {"messages": e})
+        return render(request, 'account/register.html', {"messages": e})
 
 # log user
+
+
 def logUser(request):
     # validate post data
     email = request.POST['email']
     password = request.POST['password']
-   
-    user = User.objects.get(email=email)
-    print(user.password)
-    is_correct = check_password(password, user.password)
-    print(is_correct)
-
-    if not user and is_correct:
-      return render(request, 'account/login.html', {"messages": "Incorrect email or password"})
-      
      
+    if email and password:
+      try:
+        user = User.objects.get(email=email)
+        is_correct = check_password(password, user.password)
+        
+        if not user and is_correct:
+          return render(request, 'account/login.html', {"message": "Incorrect email or password"})
+        else:
+          return redirect_to_main(str(user._id), redirect)
 
+      except User.DoesNotExist:
+          return render(request, 'account/login.html', {"message": "Incorrect email or password"})
+
+    else:
+      return render(request, 'account/login.html', {"message": "Please fill all fields"})
