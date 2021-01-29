193f60c15a1f0aee15c84f32399dabb66c8a3489
codernuub <sardarbhai6@gmail.com>
2021-01-30 01:09:05 +0530

    refractor login view

--- account/views.py
+++ account/views.py
@@ -5,58 +5,56 @@ from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 from .utils import set_cookie, verify_token, generate_token, redirect_to_main
 
-
-
 # register page
 def register(request):
-    return render(request, 'account/register.html')
+  return render(request, 'account/register.html')
 
 # login page
 def login(request):
-    # check token is valid redirect user to main application
-    if verify_token(request.COOKIES.get('token')):
-        return redirect("127.0.0.1:3000")
+  # check token is valid redirect user to main application
+  if verify_token(request.COOKIES.get('token')):
+    return redirect("127.0.0.1:3000")
 
-    return render(request, 'account/login.html')
+  return render(request, 'account/login.html')
 
 # register user
 def registerUser(request):
 
-    name = request.POST['name']
-    email = request.POST['email']
-    contact = request.POST['contact']
-    password = request.POST['password']
+  name = request.POST['name']
+  email = request.POST['email']
+  contact = request.POST['contact']
+  password = request.POST['password']
 
-    try:
-        user = User(name=name, email=email, contact=contact)
-        if password:
-            user.password = make_password(password)
-        user.full_clean()
-        user.save()
-        return redirect_to_main(user._id, redirect)
-    except Exception as e:
-        return render(request, 'account/register.html', {"messages": e})
+  try:
+    user = User(name=name, email=email, contact=contact)
+    if password:
+       user.password = make_password(password)
+    user.full_clean()
+    user.save()
+    return redirect_to_main(user._id, redirect)
+  except Exception as e:
+    return render(request, 'account/register.html', {"messages": e})
 
 # log user
-
-
 def logUser(request):
-    # validate post data
-    email = request.POST['email']
-    password = request.POST['password']
+  # validate post data
+  email = request.POST['email']
+  password = request.POST['password']
      
-    if email and password:
-      try:
-        user = User.objects.get(email=email)
-        is_correct = check_password(password, user.password)
-        
-        if not user and is_correct:
-          return render(request, 'account/login.html', {"message": "Incorrect email or password"})
-        else:
-          return redirect_to_main(str(user._id), redirect)
+  try:
+    if not email and password:
+      raise Exception("Please fill all fields")
+      
+    user = User.objects.get(email=email)
 
-      except User.DoesNotExist:
-          return render(request, 'account/login.html', {"message": "Incorrect email or password"})
+    correct = check_password(password, user.password)
+    if not correct:
+      raise Exception("Incorrect password")
+    
+    return redirect_to_main(str(user._id), redirect)
 
-    else:
-      return render(request, 'account/login.html', {"message": "Please fill all fields"})
+  except Exception as e:
+    if type(e).__name__ == "DoesNotExist":
+      e = "User not found" 
+    return render(request, 'account/login.html', {"message": e})
+
